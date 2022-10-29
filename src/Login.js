1d8a8904aa08fd38f8a5f63ee3fe1023df627b90
adelpro <47066151+adelpro@users.noreply.github.com>
2022-10-29 15:26:59 +0100

    Create Login.js

---
+++ src/Login.js
@@ -0,0 +1,44 @@
+import "./styles.css";
+import { useEffect, useRef, useState } from "react";
+export default function Login() {
+  const usernameRef = useRef();
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    console.log("handle submit");
+  };
+  useEffect(() => {
+    usernameRef.current.focus();
+  }, []);
+
+  return (
+    <div className="App">
+      <h1>Login</h1>
+      <form onSubmit={handleSubmit} className="form__container">
+        <div className="form__controls">
+          <label htmlFor="username">Username</label>
+          <input
+            ref={usernameRef}
+            type="text"
+            id="username"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+        </div>
+        <div className="form__controls">
+          <label htmlFor="password">Password</label>
+          <input
+            id="password"
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
+        <div className="form__controls">
+          <button className="button">Login</button>
+        </div>
+      </form>
+    </div>
+  );
+}
